6008b585b65ed4b2d3b22bfa79a7366f67d9ea0e
Ibad Rather <ibad.rather@zeiss.com>
2022-06-07 13:34:37 +0200

    done for now

--- Darts/8_kalman_filtering_damped_oscillation.py
+++ Darts/8_kalman_filtering_damped_oscillation.py
@@ -1,5 +1,4 @@
 # Author: Ibad Rather, ibad.rather.ir@gmail.com, June 2022
-# https://github.com/unit8co/darts/blob/master/examples/11-GP-filter-examples.ipynb
 
 import numpy as np
 import pandas as pd
@@ -10,32 +9,34 @@ from utils import clear_terminal
 from utils import *
 
 from darts import TimeSeries
+from darts.models import KalmanFilter
 
 clear_terminal()
 
 def add_white_noise(signal):
-    sigma = 0.09
+    sigma = 0.009
     return np.random.normal(signal, sigma, signal.shape[0])
 
 # Generate timestamps
-signal_length = 30
-signal_frequency = 30 # hz
+signal_length = 20
+signal_frequency = 100 # hz
 timestamps = np.linspace(0, signal_length, signal_length*signal_frequency)
 
 # Generate Sin wave
-omega = 2
-signal = np.sin(omega*timestamps)
+omega = 20
+signal = np.exp(-timestamps)*np.sin(omega*timestamps)
 
 # Adding Noise to it
 noisy_signal = add_white_noise(signal)
 plt.plot(timestamps, signal, color="r", label="Signal")
 plt.plot(timestamps, noisy_signal, color="b", label="Noisy Signal")
 plt.legend()
+plt.show()
 
 plt.close("all")
 
-
 # Now that we have our signal and its timestamps. Let's convert it into a TimeSeries
+
 # Noisy Signal will be the Input and Without Noise Signal the output
 
 # Prepare the input
@@ -44,24 +45,23 @@ input_signal = TimeSeries.from_values(noisy_signal)
 # Prepare the output
 output_signal = TimeSeries.from_values(signal)
 
-## Filtering and predicting using the Gaussian Process filter
-from sklearn.gaussian_process.kernels import ExpSineSquared, RBF
-from darts import TimeSeries
-from darts.models import GaussianProcessFilter
-
-# Infer the mean of a Gaussian Process using a periodic kernel
-
-kernel = ExpSineSquared()
-# kernel = RBF()
-
-gpf = GaussianProcessFilter(
-    kernel=kernel, alpha=0.05 / 2, n_restarts_optimizer=100
-)
-filtered_signal = gpf.filter(input_signal)
-
+# Let's visualise these TimeSeries Signals
 plt.figure(figsize=[12, 8])
-output_signal.plot(color="black", label="Orginal sine wave")
-#input_signal.plot(color="red", label="Noisy sine wave")
-filtered_signal.plot(color="blue", label="Filtered sine wave")
+input_signal.plot(label="Noisy Input")
+output_signal.plot(color="gray", label="Ideal Output")
+plt.legend()
+
+# Traning
+kf = KalmanFilter(dim_x=1)
+kf.fit(output_signal, input_signal)
+
+# Filtering
+signal_filtered = kf.filter(output_signal, input_signal)
+plt.close("all")
+plt.figure(figsize=[12, 8])
+input_signal.plot(label="Noisy Input")
+output_signal.plot(color="gray", label="Ideal Output")
+signal_filtered.plot(color="red", label="Filtered Output")
 plt.legend()
 plt.show()
+
