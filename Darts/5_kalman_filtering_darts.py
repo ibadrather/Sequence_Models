baa90093c2100bedf87fcc2d5bf8d6932a83a6ea
Ibad Rather <ibad.rather@zeiss.com>
2022-06-07 10:47:22 +0200

    added kalman filtering

---
+++ Darts/5_kalman_filtering_darts.py
@@ -0,0 +1,37 @@
+# Source: https://github.com/unit8co/darts/blob/master/examples/10-Kalman-filter-examples.ipynb
+
+from utils import *
+fix_pythonpath_if_working_locally()
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from darts import TimeSeries
+from darts.models import KalmanFilter
+from darts.utils import timeseries_generation as tg
+
+NOISE_DISTANCE = 0.1
+SAMPLE_SIZE = 200
+np.random.seed(42)
+
+# Prepare the input
+u = TimeSeries.from_values(np.heaviside(np.linspace(-5, 10, SAMPLE_SIZE), 0))
+
+# Prepare the output
+y = u * TimeSeries.from_values(1 - np.exp(-np.linspace(-5, 10, SAMPLE_SIZE)))
+
+# Add white noise to obtain the observations
+noise = tg.gaussian_timeseries(length=SAMPLE_SIZE, std=NOISE_DISTANCE)
+y_noise = y + noise
+
+plt.figure(figsize=[12, 8])
+u.plot(label="Input")
+y.plot(color="gray", label="Output")
+y_noise.plot(color="red", label="Noisy observations")
+plt.legend()
+
+# 
+
+plt.show()
+
+
